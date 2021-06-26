5579ccc154e0fea88c08a964765d00388547fda7
MiriNu <miryamnudelman@gmail.com>
2021-06-26 23:07:29 +0300

    init

---
+++ Assets/Scripts/GoToFood.cs
@@ -0,0 +1,21 @@
+﻿public class GoToFood : GAction
+{
+    public override bool PrePerform()
+    {
+
+        return true;
+    }
+
+    public override bool PostPerform()
+    {
+
+        // Inject eating state to world states
+        GWorld.Instance.GetWorld().ModifyState("Eating", 1);
+        // Patient adds himself to the queue
+        GWorld.Instance.AddPatient(this.gameObject);
+        // Inject a state into the agents beliefs
+        beliefs.ModifyState("lessHungry", 1);
+
+        return true;
+    }
+}
---
+++ Assets/Scripts/PokemonMovement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Navmesh
+using UnityEngine.AI;
+
+
+
+// THIS IS JUST A NAVMESH TEST
+
+
+public class PokemonMovement : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private Rigidbody2D rb;
+
+    //Navmesh
+    [SerializeField] private Transform target;
+    private NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Navmesh
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+        MoveToTarget(target, 0.4f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        //agent.SetDestination(target.position);
+    }
+
+    public void MoveToTarget(Transform trgt, float distance)
+    {
+        Vector3 buffer = new Vector3(distance, distance, 0);
+        agent.SetDestination(trgt.position + buffer);
+    }
+
+
+   
+}
